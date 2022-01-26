876253355423b97474fbc12c8af3e44157310524
mosabzakarneh98 <mosab.zakarneh@harri.com>
2022-01-26 14:49:53 +0200

    changes done

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+import kotlin.reflect.typeOf
+
+@OptIn(ExperimentalStdlibApi::class)
+fun b(s:String?)=s?.length
+fun vv()= println("df")
+fun v(){
+
+
+}
+fun main(args: Array<String>) {
+
+    vv()
+    val k:String?=null
+   val t:String= when (k)
+    {
+
+        null ->"5 as String"
+      else ->""
+    }
+    println(t)
+    val s="${k?.length?:b("mosa")}"
+    val w="""for (c in "dfg") 
+        |println(c) """.trimMargin()
+    if(k==null)println("mosab$s")
+
+
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
