8b19921f597bdba7847ea0b7468c21220228a030
mosabzakarneh98 <mosab.zakarneh@harri.com>
2022-02-13 16:05:54 +0200

    excercises

--- src/main/kotlin/Exercises.kt
+++ src/main/kotlin/Exercises.kt
@@ -1,4 +1,6 @@
 import java.time.Year
+import java.util.*
+
 object single{
     var name:String=""
     fun printName()= println(name)
@@ -13,6 +15,25 @@ class Exercises private constructor(val name:String){
     fun printName()= println(name)
 
 }
+data class Complex(val real:Double,val imag:Double)
+operator fun Complex.plus(com1:Complex):Complex
+{
+
+    return Complex((this.real+com1.real),(this.imag+com1.imag))
+}
+data class EntryTag(
+    val id: Long,
+    val name: String,
+    val color: String,
+    val status: String
+)
+data class EntryTagSelectable(
+   val content: EntryTag,
+   val isSelected: Boolean,
+   val title: String
+)
+
+
 
 fun main()
 {
@@ -51,7 +72,57 @@ fun main()
     println(decToBinOrBintoDec(10))
     println(checkValue(arrayOf(1,2,3,4),6))
     println(joinTwoLists(listOf(1,2,3,4), listOf(5,6,7,8)))
-    //println(mapOf(1 to 4,2 to 10, 3 to 2,4 to 7))
+    println(sortMapByValue(mapOf("1" to 4,"2" to 10, "3" to 2,"4" to 7)))
+    val complex=Complex(2.0,3.0)+Complex(-1.0,-1.0)
+    println("${complex.real }+${complex.imag}j ")
+    println(isStringANummeric("12a4"))
+    val list = mutableListOf<EntryTag>().apply {
+        add(EntryTag(9, "Ahmad","#0088dd","ACTIVE"))
+        add(EntryTag(5, "rami","#005285","ACTIVE"))
+        add(EntryTag(2, "Sami","#277ed5","ACTIVE"))
+        add(EntryTag(7, "Rami","#ffffff","ACTIVE"))
+        add(EntryTag(21, "ah","#409bdf","ACTIVE"))
+        add(EntryTag(43, "Al","#55bcfc","ACTIVE"))
+        add(EntryTag(76, "Anas","#0079c4","ACTIVE"))
+        add(EntryTag(214, "mais","#0088dd","ACTIVE"))
+        add(EntryTag(214, "mais","#5c6991","ACTIVE"))
+    }
+    println(sortList(list))
+    val selectedEntryTagId = 43
+    val inputList = mutableListOf<EntryTag>().apply {
+        add(EntryTag(9, "Ahmad","#0088dd","ACTIVE"))
+        add(EntryTag(5, "rami","#005285","ACTIVE"))
+        add(EntryTag(2, "Sami","#277ed5","ACTIVE"))
+        add(EntryTag(2, "Sami","#277ed5","ACTIVE"))
+        add(EntryTag(7, "Rami","#ffffff","ACTIVE"))
+        add(EntryTag(21, "ah","#409bdf","ACTIVE"))
+        add(EntryTag(43, "Al","#55bcfc","ACTIVE"))
+        add(EntryTag(43, "Al","#55bcfc","ACTIVE"))
+        add(EntryTag(76, "Anas","#0079c4","ACTIVE"))
+        add(EntryTag(214, "mais","#0088dd","ACTIVE"))
+        add(EntryTag(214, "mais","#5c6991","ACTIVE"))
+    }
+    println(createList(inputList,selectedEntryTagId))
+    takePrintFun( {println(it)},"mosab")
+    val list2= mutableListOf<Int>()
+    for (i in 1..12)
+    {
+        list2.add(i)
+        list2.add(i)
+    }
+    println(removeDuplicates(list2))
+    println(twoArrayIsEqual(arrayOf(1,2,3,4), arrayOf(1,2,3,4)))
+    println(removeOddOrNegativeFromList(listOf(1,-1,3,4,7,-3)))
+    println(removeNullFromList(listOf(null,1,null,2,null)))
+    println(reverseList(listOf(1,2,3,5)))
+    println(stringToArray("mosab"))
+
+
+
+
+
+
+
 
 
 
@@ -164,8 +235,66 @@ fun <T>joinTwoLists(list1:List<T>,list2:List<T>):List<T>{
 
     return list1.plus(list2)
 }
-fun <K,V>sortMapByValue(map:Map<K,V>): Map<K,V>?
+fun sortMapByValue(map:Map<String,Int>): Map<String,Int>
 {
-  return null
+  return map.toList().sortedBy { it.second}.toMap()
+}
+fun addTwoComplexNum(num1:Double,num2:Double):Double
+{
+   return num1
+}
+fun isStringANummeric(string:String):Boolean
+{
+    for(i in string)
+        if(!i.isDigit()) return false
+
+    return true
+}
+fun sortList(list:MutableList<EntryTag>):MutableList<EntryTag>
+{
+
+   return list.sortedBy { it.name }.toMutableList()
+}
+fun createList(list:List<EntryTag>,selectedId:Int):List<EntryTagSelectable>
+{
+    val mutableList= mutableListOf<EntryTagSelectable>()
+    for (i in list)
+        if(i.id!=selectedId.toLong())
+        mutableList.add(EntryTagSelectable(i,false,"something"))
+    else
+            mutableList.add(EntryTagSelectable(i,true,"something"))
+    return mutableList
+}
+fun takePrintFun(funPrint: (String) -> Unit,messageToPrint:String):Unit
+{
+funPrint(messageToPrint)
+}
+fun <T>removeDuplicates(list:MutableList<T>):List<T>
+{
+
+return list.distinct()
+}
+fun <T>twoArrayIsEqual(a1:Array<T>,a2:Array<T>):Boolean
+{
+    return a1.contentEquals(a2)
+}
+fun removeOddOrNegativeFromList (list:List<Int> ) : List<Int>
+{
+
+return list.filter { it%2==0 && it>0 }
+}
+fun <T> removeNullFromList(list:List<T?>):List<T?>
+{
+
+    return list.toMutableList().apply {  removeIf { it==null }}.toList()
+
+}
+fun <T> reverseList(list:List<T>):List<T>
+{
+    return list.reversed()
+}
+fun stringToArray(string: String):CharArray
+{
+    return string.toCharArray()
 }
 
